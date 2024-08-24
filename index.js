ea7bc6d31b2179c534adb520fb7eefe9940eadeb
Sam Treadwell <treadwell.sa@northeastern.edu>
2024-08-24 09:58:52 -0400

    Dark/Light mode animations working

--- index.js
+++ index.js
@@ -1,36 +1,45 @@
-
 const lightDarkMode = document.getElementById("light-dark-mode");
 const modeFlip = document.getElementById("mode-flip");
-const modeIcon = document.getElementById("mode-icon")
+const modeIcon = document.getElementById("mode-icon");
 
 let isDarkMode = true;
 let rotationInProgress = false;
 let currentDegrees = 0;
+let x = 0;
 
 lightDarkMode.addEventListener("click", flip);
 
-function flip(){
-    if(rotationInProgress) return;
+function flip() {
+    if (rotationInProgress) return;
 
     rotationInProgress = true;
 
     const targetDegrees = currentDegrees === 0 ? 180 : 0;
     const rotationDirection = currentDegrees === 0 ? 1 : -1;
+    const maxX = lightDarkMode.clientWidth - modeIcon.clientWidth;
+    const targetX = isDarkMode ? maxX : 0;
 
-    let timerId = setInterval(frame, 1);
+    let timerId = setInterval(frame, 1); 
     let degrees = currentDegrees;
 
-    function frame(){
-        if(degrees === targetDegrees){
+    function frame() {
+        if (Math.abs(degrees - targetDegrees) < 1 && Math.abs(x - targetX) < 1) {
             clearInterval(timerId);
             rotationInProgress = false;
             currentDegrees = targetDegrees;
-            modeIcon.style.transform = `rotateY(${currentDegrees})`;
+            isDarkMode = !isDarkMode;
             return;
-        }
-        else{
-            degrees += rotationDirection;
+        } else {
+            if (degrees !== targetDegrees) {
+                degrees += rotationDirection * 1.5;
+                degrees = Math.max(0, Math.min(180, degrees))
+            }
+            if (x !== targetX) {
+                x += isDarkMode ? 2 : -2; 
+                x = Math.max(0, Math.min(maxX, x));
+            }
             modeFlip.style.transform = `rotateY(${degrees}deg)`;
+            modeIcon.style.transform = `translateX(${x}px)`;
         }
     }
 }
